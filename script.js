982e999ece4ef327a6a11c3ddc9281a9f6cd9d5b
javier_303 <javiercastaneda303@gmail.com>
2021-09-23 20:14:07 +0100

    _

---
+++ script.js
@@ -0,0 +1,23 @@
+
+ let inputA = document.getElementById("inputA");
+ let inputB = document.getElementById("inputB");
+ 
+ let suma = document.getElementById("suma");
+ 
+ let resta = document.getElementById("resta");
+ let multiplicacion = document.getElementById("multiplicacion");
+ let division = document.getElementById("division");
+  console.log(inputA.value);
+ 
+  console.log("hola colega")
+
+ suma.addEventListener("click", function(){console.log("hola desde suma")});
+/* 
+ document.getElementById("suma").addEventListener("click", function() {
+    alert("hola desde sumaaaa");
+  }, true);
+ */
+
+
+
+/* document.getElementById("myDiv").addEventListener("click", myFunction, true); */
